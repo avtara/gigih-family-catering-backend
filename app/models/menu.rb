214820dccc1b8e6480ab56795e2159b87731a115
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-04-23 03:33:23 +0700

    Add (Green): Implement add menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,6 +2,9 @@ class Menu < ApplicationRecord
   belongs_to :category
 
   def self.add_menu(name,description,price,category)
+    if category.nil?
+      return
+    end
     category = Category.where(:name => category).first_or_create!
     Menu.new(
       name: name,
