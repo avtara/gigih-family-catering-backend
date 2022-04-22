bb02823810a65235d9e0ff22f59be9e3bded4c2f
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-04-23 02:45:07 +0700

    test (Red): Make test for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name' do
+    category = Category.where(
+      :name => "Indonesia"
+    ).first_or_create!
+
+    expect(category).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    category = Category.new(name: nil)
+    expect(category.valid?).to be false
+  end
 end
