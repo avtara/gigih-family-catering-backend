4f4c5e4be3925ef9550b96173c2a4c2d5bdf02b0
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-04-23 03:32:26 +0700

    Test (Red): Added test add menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,13 @@
 class Menu < ApplicationRecord
   belongs_to :category
+
+  def self.add_menu(name,description,price,category)
+    category = Category.where(:name => category).first_or_create!
+    Menu.new(
+      name: name,
+      description: description,
+      price: price,
+      category: category
+    )
+  end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name and a description' do
+    menu = Menu.add_menu(
+      "Nasi Uduk",
+      'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      15000.0,
+      "Padang"
+    )
+    expect(menu).to be_valid
+  end
+
+  it 'is invalid withou a category' do
+    menu = Menu.add_menu(
+      "Nasi Uduk",
+      'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      15000.0,
+      nil
+    )
+    expect(menu).nil?
+  end
+  # it 'is invalid without a name' do
+  #   menu = Menu.new(
+  #     name: nil,
+  #     description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+  #     price: 15000.0
+  #   )
+  #
+  #   menu.valid?
+  #
+  #   expect(menu.errors[:name]).to include("can't be blank")
+  # end
+  #
+  # it "is invalid with a duplicate name" do
+  #   menu1 = Menu.create(
+  #     name: "Nasi Uduk",
+  #     description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+  #     price: 10000.0
+  #   )
+  #
+  #   menu2 = Menu.new(
+  #     name: "Nasi Uduk",
+  #     description: "Just with a different description.",
+  #     price: 10000.0
+  #   )
+  #
+  #   menu2.valid?
+  #
+  #   expect(menu2.errors[:name]).to include("has already been taken")
+  # end
 end
